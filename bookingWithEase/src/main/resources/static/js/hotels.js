5c9ee8352963dec3b9d50cb5be030b545ab46ce1
Ana Mijailović <anaam1997@gmail.com>
2019-03-26 21:06:45 +0100

    Hotels view

--- bookingWithEase/src/main/resources/static/js/hotels.js
+++ bookingWithEase/src/main/resources/static/js/hotels.js
@@ -9,11 +9,27 @@ function getHotels()
 		url : "/hotels",
 		type : "GET",
 		dataType : 'json',
-		success : function(response) {
-			
-		},
+		success : fillTable,
 		error : function(response) {
 			alert("Something went wrong! :(");
 		}
 	});
+}
+
+function fillTable(data) {
+	var hotels = data == null ? []
+			: (data instanceof Array ? data : [ data ]);
+	$('#hotelsTable').empty();
+	$('#hotelsTable').append('<tr><th>Name</th><th>Address</th><th>Description</th></tr>');
+	$.each(hotels, function(index, hotel) {
+
+		var tr = $('<tr></tr>');
+		tr.append('<td>' + hotel.name + '</td>' + '<td>'
+				+ hotel.address + '</td>' + '<td>'
+				+ hotel.description + '</td>');
+
+		$('#hotelsTable').append(tr);
+
+	});
+
 }
