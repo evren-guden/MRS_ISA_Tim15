b69acc4c62fad74c6168a4c3e9f3c2640a235af0
BojanaZoric <bjnzrc@gmail.com>
2019-06-11 19:38:57 +0200

    Brza rezervacija vozila

--- bookingWithEase/src/main/java/rs/travel/bookingWithEase/controller/QuickVehicleReservationController.java
+++ bookingWithEase/src/main/java/rs/travel/bookingWithEase/controller/QuickVehicleReservationController.java
@@ -9,21 +9,35 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 import rs.travel.bookingWithEase.dto.QuickVehicleReservationDTO;
 import rs.travel.bookingWithEase.model.QuickVehicleReservation;
+import rs.travel.bookingWithEase.model.RegisteredUser;
+import rs.travel.bookingWithEase.model.User;
+import rs.travel.bookingWithEase.model.VehicleReservation;
 import rs.travel.bookingWithEase.service.QuickVehicleReservationService;
+import rs.travel.bookingWithEase.service.UserService;
+import rs.travel.bookingWithEase.service.VehicleReservationService;
 
 @Controller
-@RequestMapping(value = "rentacars/{racId}/quickVehicleReservations")
+@RequestMapping(value = "/quickVehicleReservations")
 public class QuickVehicleReservationController {
 
 	@Autowired
 	private QuickVehicleReservationService quickVehService;
+	
+	@Autowired
+	private UserService userService;
 
+	@Autowired
+	private VehicleReservationService vehResService;
+	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Collection<QuickVehicleReservation>> getAll() {
 
@@ -51,6 +65,28 @@ public class QuickVehicleReservationController {
 		for (QuickVehicleReservation quickVehicleReservation : qvr) {
 			quickVehService.update(quickVehicleReservation);
 		}
+		
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/{qvrId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> reserve(@RequestBody Long userId, @PathVariable("qvrId") Long qvrId){
+		
+		QuickVehicleReservation qvr = quickVehService.getOne(qvrId);
+		
+		User user = userService.findOne(Long.valueOf(userId));
+		
+		VehicleReservation res = new VehicleReservation();
+		
+		res.setCheckInDate(qvr.getCheckInDate());
+		res.setCheckOutDate(qvr.getCheckOutDate());
+		res.setTotalPrice(qvr.getFinalPrice());
+		res.setUser((RegisteredUser) user);
+		res.setVehicle(qvr.getVehicle());
+		
+		vehResService.save(res);
+		
+		quickVehService.delete(qvrId);
 		
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
