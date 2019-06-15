dd942456b41e3292499269e34db5271d03af1634
BojanaZoric <bjnzrc@gmail.com>
2019-06-15 14:51:12 +0200

    Ocenjivanje frontend

--- bookingWithEase/src/main/java/rs/travel/bookingWithEase/repository/IFlightReservationRepository.java
+++ bookingWithEase/src/main/java/rs/travel/bookingWithEase/repository/IFlightReservationRepository.java
@@ -1,6 +1,9 @@
 package rs.travel.bookingWithEase.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import rs.travel.bookingWithEase.model.FlightReservation;
 import rs.travel.bookingWithEase.model.RegisteredUser;
@@ -9,4 +12,6 @@ public interface IFlightReservationRepository extends JpaRepository<FlightReserv
 
 	FlightReservation findByFUserAndFlightId(RegisteredUser user, Long flightId);
 
+	@Query("SELECT fr FROM FlightReservation fr WHERE fr.fUser.id = ?1 order by checkInDate desc")
+	List<FlightReservation> findByUser(Long userId);
 }
